afa13eaec728fc27fa283d2abeb74539a1daba6c
Pranshu Malik <pranshumalik14@gmail.com>
2020-12-12 02:09:05 -0500

    day4 running; needs debugging!

--- day4/day4.jl
+++ day4/day4.jl
@@ -9,8 +9,9 @@ Parsing and checking against only required fields in the string according to
 the respective criteria; a bunch of efficient string operations and ways to do them.
 """
 
-abstract type P <: AbstractString end
-abstract type S <: AbstractSet{AbstractString} end
+# shorter names for types
+P = String
+S = Set{String}
 
 # passport check criteria functions
 let 
@@ -19,7 +20,7 @@ let
     # returns true if passport contains all required fields
     global function check_fields_contained(passport::P, optional_fields::S)
         # initialize fields to be checked
-        reqd_fields = setdiff(reqd_fields, optional_fields)
+        reqd_fields = setdiff(check_fields, optional_fields)
 
         return all([contains(passport, field) for field ∈ reqd_fields])
     end
@@ -27,14 +28,14 @@ let
     # returns true if passport contains all required fields with valid values
     global function check_fields_valid(passport::P, optional_fields::S)
         # initialize fields to be checked
-        reqd_fields = setdiff(reqd_fields, optional_fields)
+        reqd_fields = setdiff(check_fields, optional_fields)
 
         # parse fields into a {field, value} dict and declare
         field_dict = Dict{String,Any}()
         parse_field_values!(field_dict, passport, reqd_fields)
 
         # enforce evaluation according to field
-        fields_valid = map(reqd_fields) do field
+        fields_valid = map(collect(reqd_fields)) do field
             if field == "byr"
                 check = (v) -> (1920 ≤ v ≤ 2002)
             elseif field == "iyr"
@@ -59,10 +60,11 @@ let
             if !haskey(field_dict, field) 
                 return false
             else
+                println("Checking $field with value $(field_dict[field]): $(check(field_dict[field]))")
                 return check(field_dict[field])
             end
         end
-
+        println("Passport is $(all(fields_valid) ? "" : "not") valid!\n####")
         return all(fields_valid)
     end
 
@@ -70,13 +72,20 @@ let
     function parse_field_values!(field_dict::Dict{String,Any}, passport::P, required_fields::S)
         # parse values one-by-one according to type expected
         for field ∈ required_fields
+            # find field; continue to next if not found
+            find = findlast(field, passport)
+            if find === nothing 
+                continue
+            end
+
             # get the value (match) till '\n' or ' ' after the field string
-            val_index = findlast(field, passport).stop + 1
+            val_index = find.stop + 1
             val = match(r"(?<=:)\w*", passport[val_index:end]).match
             
             # parse values as types from string if necessary
             if field == "hgt"
-                val = uparse(endswith(val, "in") ? val * "ch" : val)
+                if !endswith(val, r"in|cm") continue end
+                val = uparse(endswith(val, "in") ? val * "ch" : val) # todo: check if unit parsed was of type length
             elseif field ∈ ["byr", "iyr", "eyr"]
                 val = parse(Int64, val) # add as int
             end
@@ -98,7 +107,7 @@ end
 # run all parts
 function main()
     # parse input into array of passport info strings
-    psprt_strs = split(read("input.txt", String), "\n\n") # can fix by String.() but ugly. any tips?
+    psprt_strs = String.(split(read("input.txt", String), "\n\n"))
 
     # run
     @show num_valid_passports(psprt_strs, check_fields_contained; optional_fields=Set(["cid"]))
