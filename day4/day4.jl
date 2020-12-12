9c5ba5dda4edb55dad87c470b4c1021967d20d0c
Pranshu Malik <pranshumalik14@gmail.com>
2020-12-12 18:07:48 -0500

    day 4 corrected!!

--- day4/day4.jl
+++ day4/day4.jl
@@ -10,8 +10,8 @@ the respective criteria; a bunch of efficient string operations and ways to do t
 """
 
 # shorter type names
-P = AbstractString
-S = AbstractSet{<:AbstractString}
+const P = AbstractString
+const S = AbstractSet{<:AbstractString}
 
 # passport check criteria functions
 const check_fields = Set{String}(["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"])
@@ -42,7 +42,7 @@ function check_fields_valid(passport::P, optional_fields::S)
         elseif field == "eyr"
             check = (v) -> (2020 ≤ v ≤ 2030)
         elseif field == "hgt"
-            check = (v) -> (150u"cm" ≤ v ≤ 76u"inch")
+            check = (v) -> (59u"inch" ≤ v ≤ 76u"inch")
         elseif field == "hcl"
             check = (v) -> (length(v) == 7 && occursin(r"(?<=#)[a-f0-9]*$", v))
         elseif field == "ecl"
@@ -99,7 +99,7 @@ end
 function num_valid_passports(passports::Vector{<:P},
     criteria::Function; optional_fields::S=Set{String}())
     # result is total correct passports
-    return sum([criteria(psprt, optional_fields) for psprt ∈ passports])
+    return count([criteria(psprt, optional_fields) for psprt ∈ passports])
 end
 
 # run all parts
