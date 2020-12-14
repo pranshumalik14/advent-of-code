aba6c8a8cca57d96c368def3688487f51a55b8a0
Pranshu Malik <pranshumalik14@gmail.com>
2020-12-14 17:48:14 -0500

    day 6 complete; part 1 broken

--- day6/day6.jl
+++ day6/day6.jl
@@ -10,20 +10,20 @@ Set operations: union (∪) and intersect (∩).
     return group_str |> Set |> length
 end
 
-# returns number of entries in onsensus in a group
+# returns number of entries in consensus in a group
 @inline function num_consensus(group_str::AbstractString)
-    return ∩(group_str |> split .|> Set) |> length
+    return group_str |> split .|> Set |> x -> reduce(intersect, x) |> length
 end
 
 # returns sum of unique entries for all groups
-function sum_unique(groups_str::Vector{<:AbstractString})
-    # clump groups to one line before adding to set
-    return groups_str .|> x -> replace(x, "\n" => "") .|> num_unique |> sum
+function sum_unique(group_strs::Vector{<:AbstractString})
+    # clump groups to one line before adding to set (removes the need for union)
+    return group_strs .|> x -> replace(x, "\n" => "") .|> num_unique |> sum
 end
 
 # returns sum of consensus entries for all groups
-function sum_consensus(groups_str::Vector{<:AbstractString})
-    return groups_str .|> num_consensus |> sum
+function sum_consensus(group_strs::Vector{<:AbstractString})
+    return group_strs .|> num_consensus |> sum
 end
 
 # run all parts
